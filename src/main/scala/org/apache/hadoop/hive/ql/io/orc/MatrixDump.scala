9fd6bc97ff1ee37c1711964f31a4444389012ecb
Vivek Y S <vivek.ys@gmail.com>
2014-02-28 23:44:44 +0530

    Debug

--- src/main/scala/org/apache/hadoop/hive/ql/io/orc/MatrixDump.scala
+++ src/main/scala/org/apache/hadoop/hive/ql/io/orc/MatrixDump.scala
@@ -2,8 +2,8 @@ package org.apache.hadoop.hive.ql.io.orc
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
-import scala.collection.JavaConversions._
-import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.WritableLongObjectInspector
+import org.apache.hadoop.hive.ql.io.orc.OrcStruct.OrcStructInspector
 
 /**
  * Created with IntelliJ IDEA.
@@ -21,15 +21,31 @@ object MatrixDump {
       val path = new Path(filename)
       val reader = OrcFile.createReader(path.getFileSystem(conf), path, conf)
       val rows = reader.rows(null).asInstanceOf[RecordReaderImpl]
+
       println("Rows: " + reader.getNumberOfRows)
       println("Compression: " + reader.getCompression)
+
       if (reader.getCompression != CompressionKind.NONE) {
-        System.out.println("Compression size: " + reader.getCompressionSize)
+        println("Compression size: " + reader.getCompressionSize)
       }
       println("Type: " + reader.getObjectInspector.getTypeName)
 
-      val soi = reader.getObjectInspector.asInstanceOf[StructObjectInspector]
-      println(soi)
+      val soi = reader.getObjectInspector.asInstanceOf[OrcStructInspector]
+      val sb = new StringBuilder
+      val fields = soi.getAllStructFieldRefs
+
+      var row : OrcStruct = rows.next(null).asInstanceOf[OrcStruct]
+      while (rows.hasNext) {
+        row = rows.next(row).asInstanceOf[OrcStruct]
+        for (i <- 0 until fields.size) {
+          if (i > 0) {
+            sb.append(",");
+          }
+          sb.append(row.getFieldValue(i)).append("\t")
+//          sb.append(fields.get(i).getFieldObjectInspector().asInstanceOf[WritableLongObjectInspector].get(row));
+        }
+      }
+      println(sb)
       rows.close();
     }
   }
